9825cdce6e80d05898f2b7c8f99a8f77e30c444c
Ch3zZz <60223541+Ch3zZz@users.noreply.github.com>
2020-09-17 11:04:45 +0300

    Update and rename 1..py to main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from pypresence import Presence
 import time
 
-client_id = '702852927450120282'  # Fake ID, put your real one here
+client_id = ''  # put your real one client id
 RPC = Presence(client_id)  # Initialize the client class
 RPC.connect() # Start the handshake loop
 
 
-anime = input("Anime vvedi svoloch: ")
-seriya = int(input("Na kakoi serii: "))
-vsego = int(input("Vsego Skok: "))
+anime = input("Какое аниме смотришь: ")
+seriya = int(input("На какой серии: "))
+vsego = int(input("Сколько всего: "))
 while True:
 	print(RPC.update(state=anime, details="Смотрит Аниме", party_size=[seriya, vsego])) # Can only update rich presence every 15 seconds
 	time.sleep(1)
